1f40fb794e88f27db7ab54e1aa54098859d2e342
Stjepan Strmečki <stjepan.strmecki01@gmail.com>
2023-01-02 14:58:35 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, } from 'react';
+import './App.css';
+
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  //states
+  const [inputText, setInputText] = useState(""); 
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //USE effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos,status]);
+
+  //methods
+  const filterHandler = () =>{
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Todays tasks</h1>
+      </header>
+      <Form inputText = {inputText} todos = {todos} setTodos = {setTodos} setInputText = {setInputText} setStatus={setStatus}/>
+      <TodoList setTodos = {setTodos} todos = {todos} filteredTodos = {filteredTodos}/>
+    </div>
+  );
+}
+
+export default App;
